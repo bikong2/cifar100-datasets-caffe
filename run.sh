764130d233a136974c95697a48a3f43dd229c443
lixihua <lixihua9@126.com>
2017-07-15 17:43:08 +0800

    plt problem solved

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #python scripts/convert_cifar100_lmdb.py
 #python scripts/read_lmdb.py
 #python scripts/compute_mean.py
-/home/lixihua/tools/caffe/build/tools/caffe train --solver=cifar100/normal/solver.prototxt
+
+TOOLS=/home/lixihua/tools
+#$TOOLS/caffe/build/tools/caffe train --solver=cifar100/normal/solver.prototxt 2>&1 |tee cifar100.log
+#$TOOLS/caffe/tools/extra/parse_log.sh cifar100.log
+
+$TOOLS/caffe/tools/extra/plot_training_log.py.example -h
+$TOOLS/caffe/tools/extra/plot_training_log.py.example 6 train.png cifar100.log
+$TOOLS/caffe/tools/extra/plot_training_log.py.example 2 test.png cifar100.log
